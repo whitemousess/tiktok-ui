d4bda350735688c3286cc60e369775ba986a6c8e
Chuột Bạch <94060884+whitemousess@users.noreply.github.com>
2023-04-29 15:10:47 +0700

    The END header

--- src/components/Popper/Menu/Menu.js
+++ src/components/Popper/Menu/Menu.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 import { useState } from 'react';
@@ -12,11 +12,10 @@ const cx = classNames.bind(styles);
 
 const defaultFn = () => {};
 
-function Menu({ children, items = [] ,hideOnClick = false, onChange = defaultFn}) {
+function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn }) {
     // dùng useState để khai báo history mặc định
     const [history, setHistory] = useState([{ data: items }]);
-    
-    
+
     // trở về popper trước
     const current = history[history.length - 1];
 
@@ -40,35 +39,37 @@ function Menu({ children, items = [] ,hideOnClick = false, onChange = defaultFn}
         });
     };
 
-    return (
+    const handleBack = () => {
+        setHistory((prev) => prev.slice(0, prev.length - 1));
+    };
 
+    const renderResult = (attrs) => (
+        <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+            {/* PopperWrapper là items trong tippy */}
+            <PopperWrapper className={cx('menu-popper')}>
+                {history.length > 1 && <Header title={current.title} onBack={handleBack} />}
+                <div className={cx('menu-body')}> {renderItems()}</div>
+            </PopperWrapper>
+        </div>
+    );
+
+    // reset to first page
+    const handleReset = () => {
+        setHistory((prev) => prev.slice(0, 1));
+    };
+
+    return (
         // Tippy thay cho thanh menu item
         <Tippy
             interactive
             delay={[0, 700]}
-            offset={[12,8]}
+            offset={[12, 8]}
             // disable hide menu when click avatar
-             hideOnClick={hideOnClick}
+            hideOnClick={hideOnClick}
             placement="bottom-end"
-            render={(attrs) => (
-                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                   {/* PopperWrapper là items trong tippy */}
-                    <PopperWrapper className={cx('menu-popper')}>
-                        {history.length > 1 && (
-                            <Header
-                                title= {current.title}
-                                onBack={() => {
-                                    setHistory((prev) => prev.slice(0, prev.length - 1));
-                                }}
-                            />
-                        )}
-                       <div className={cx('menu-body')}> {renderItems()}</div>
-                    </PopperWrapper>
-                </div>
-            )}
-
+            render={renderResult}
             // trở về phần tử đầu tiên khi hover ra ngoài
-            onHide = {() => setHistory((prev) => prev.slice(0, 1)) }
+            onHide={handleReset}
         >
             {children}
         </Tippy>
@@ -81,6 +82,6 @@ Menu.propTypes = {
     items: PropTypes.array,
     hideOnClick: PropTypes.bool,
     onChange: PropTypes.func,
-}
+};
 
 export default Menu;
