148ddf717f94579f9d82cd483095306338b18a46
Chuột Bạch <94060884+whitemousess@users.noreply.github.com>
2023-05-02 13:04:29 +0700

    a

--- src/assets/images/index.js
+++
@@ -1,6 +0,0 @@
-const images = {
-    logo: require('~/assets/images/logo.svg').default,
-    avatarDefault: require('~/assets/images/avatarDefault.jpg')
-};
-
-export default images;
--- src/pages/Coin/Coin.js
+++
@@ -1,5 +0,0 @@
-function Coin() {
-    return <h2>Coin page</h2>;
-}
-
-export default Coin;
